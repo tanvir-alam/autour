7f6358d7e3631c790bb596f345b010be0b72e394
Tanvir Alam <tanvir@Tanvirs-MBP.lan>
2016-05-03 13:22:03 -0400

    fixed angular routing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'public'));
+app.set('views', './public');
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 
--- public/app.js
+++ public/app.js
@@ -1,5 +1,23 @@
 var app = angular.module('autour', ['ngRoute']);
 
+// create the controller and inject Angular's $scope
+app.controller('mainController', function($scope) {
+
+    // create a message to display in our view
+    $scope.message = "Give us your budget and we'll tell you where you can fly to!";
+});
+
+app.controller('searchController', function($scope) {
+
+    // create a message to display in our view
+    $scope.cities = "List of cities goes here";
+});
+
 app.config(function($routeProvider) {
-    
+    $routeProvider
+        
+        .when('/search', {
+            templateUrl : 'partials/search.html',
+            controller  : 'searchController'
+        });
 });

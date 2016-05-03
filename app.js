3a6718006c746dbfa7af3f2c12017c257151825e
Tanvir Alam <tanvir@Tanvirs-MBP.lan>
2016-05-02 21:50:59 -0400

    modified directory structure for angularjs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 

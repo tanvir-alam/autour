1abd2e6c529952b567ffaea0a2121a2aa0c067e3
Tanvir Alam <tanvir@Tanvirs-MBP.lan>
2016-05-05 17:00:08 -0400

    added form submission AngularJS routing

--- public/app.js
+++ public/app.js
@@ -1,10 +1,29 @@
 var app = angular.module('autour', ['ngRoute']);
 
 // create the controller and inject Angular's $scope
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $http) {
 
     // create a message to display in our view
     $scope.message = "Give us your budget and we'll tell you where you can fly to!";
+    
+    $scope.submit = function() {
+        $http.get('/search?origin=' + $scope.info.origin +
+            '&departuredate=' + $scope.info.departuredate +
+            '&returndate=' + $scope.info.returndate +
+            '&maxfare=' + $scope.info.maxfare)
+        .success(function(data) {
+            $scope.results = data;
+            $scope.data = data.info;
+            if ($scope.results.status) {
+                $scope.fareinfo = JSON.parse($scope.data).FareInfo;
+            } else {
+                $scope.error = JSON.parse($scope.data.data).message;
+            }
+        })
+        .error(function(error) {
+            $scope.error = JSON.parse(error.data).message;
+        });
+    };
 });
 
 app.controller('searchController', function($scope) {
